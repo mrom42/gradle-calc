b307cbf2c98fad0a2677179c2b6a22422237b565
mro <mro@brik.fritz.box>
2019-02-15 20:18:15 +0000

    Add Spring Boot skeleton

---
+++ src/main/java/com/mro/gradlecalc/GradleCalcApplication.java
@@ -0,0 +1,14 @@
+package com.mro.gradlecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCalcApplication.class, args);
+	}
+
+}
+
