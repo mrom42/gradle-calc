01554e501030a47927f96bd1a0b681fba115e177
mrom42 <martin.romisch@gmail.com>
2019-02-18 09:25:37 +0000

    gradle code check

--- src/main/java/com/mro/gradlecalc/Calculator.java
+++ src/main/java/com/mro/gradlecalc/Calculator.java
@@ -1,5 +1,8 @@
 package com.mro.calculator;
 import org.springframework.stereotype.Service;
+/**
+* Main Service
+*/
 @Service
 public class Calculator {
 int sum(int a, int b) {
--- src/main/java/com/mro/gradlecalc/GradleCalcApplication.java
+++ src/main/java/com/mro/gradlecalc/GradleCalcApplication.java
@@ -3,6 +3,9 @@ package com.mro.gradlecalc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Main Spring Application.
+*/
 @SpringBootApplication
 public class GradleCalcApplication {
 
